14de394f2683a0471ea73e008feb1cdb86783d8e
RustFisher <rustfisher@foxmail.com>
2019-12-14 21:50:13 +0800

    recyclerView点击事件

---
+++ app/src/main/java/com/rustfisher/tutorial2020/recyler/OnItemClickListener.java
@@ -0,0 +1,9 @@
+package com.rustfisher.tutorial2020.recyler;
+
+public interface OnItemClickListener {
+
+    void onItemClick(Character c);
+
+    void onItemLongClick(Character c);
+
+}
--- app/src/main/java/com/rustfisher/tutorial2020/recyler/RecyclerViewDemoActivity.java
+++ app/src/main/java/com/rustfisher/tutorial2020/recyler/RecyclerViewDemoActivity.java
@@ -6,10 +6,12 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.rustfisher.tutorial2020.R;
 
@@ -38,12 +40,33 @@ public class RecyclerViewDemoActivity extends AppCompatActivity {
         letterReView.setAdapter(mLetterAdapter);
         letterReView.setLayoutManager(new LinearLayoutManager(this, RecyclerView.VERTICAL, false));
 
+        mLetterAdapter.setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(Character c) {
+                Toast.makeText(getApplicationContext(), "Click " + c, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onItemLongClick(Character c) {
+                Toast.makeText(getApplicationContext(), "Long click " + c, Toast.LENGTH_LONG).show();
+            }
+        });
+
+        mLetterAdapter.setAbsOnItemClick(new AbsOnItemClick() {
+            @Override
+            public void onClick(char c) {
+                Log.d("rustApp", "[abs] onClick: " + c);
+            }
+        });
+
     }
 
 
     private class LetterAdapter extends RecyclerView.Adapter<VH> {
 
         private List<Character> dataList;
+        private OnItemClickListener onItemClickListener;
+        private AbsOnItemClick absOnItemClick;
 
         public LetterAdapter(List<Character> dataList) {
             this.dataList = dataList;
@@ -57,23 +80,57 @@ public class RecyclerViewDemoActivity extends AppCompatActivity {
 
         @Override
         public void onBindViewHolder(@NonNull VH holder, int position) {
-            Character c = dataList.get(position);
+            final Character c = dataList.get(position);
             holder.tv1.setText(c.toString());
             holder.tv2.setText(String.valueOf(Integer.valueOf(c)));
+            holder.item.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (onItemClickListener != null) {
+                        onItemClickListener.onItemClick(c);
+                    }
+                    if (absOnItemClick != null) {
+                        absOnItemClick.onClick(c);
+                    }
+                }
+            });
+            holder.item.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    if (onItemClickListener != null) {
+                        onItemClickListener.onItemLongClick(c);
+                    }
+                    if (absOnItemClick != null) {
+                        absOnItemClick.onLongClick(c);
+                    }
+                    return true;
+                }
+            });
         }
 
         @Override
         public int getItemCount() {
             return dataList.size();
         }
+
+
+        public void setOnItemClickListener(OnItemClickListener onItemClickListener) {
+            this.onItemClickListener = onItemClickListener;
+        }
+
+        public void setAbsOnItemClick(AbsOnItemClick absOnItemClick) {
+            this.absOnItemClick = absOnItemClick;
+        }
     }
 
     private class VH extends RecyclerView.ViewHolder {
+        View item;
         TextView tv1;
         TextView tv2;
 
         public VH(@NonNull View itemView) {
             super(itemView);
+            item = itemView;
             tv1 = itemView.findViewById(R.id.tv1);
             tv2 = itemView.findViewById(R.id.tv2);
         }
