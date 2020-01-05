f17a3b62e21a6a1739926d47e21e2181f99ff7df
RustFisher <rustfisher@foxmail.com>
2020-01-05 13:10:05 +0800

    drawable

--- app/src/main/java/com/rustfisher/tutorial2020/MainActivity.java
+++ app/src/main/java/com/rustfisher/tutorial2020/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.View;
 import com.rustfisher.tutorial2020.animation.AnimationDemoActivity;
 import com.rustfisher.tutorial2020.linear.LinearGuideAct;
 import com.rustfisher.tutorial2020.recycler.ReGuideAct;
+import com.rustfisher.tutorial2020.style.LayoutBackgroundDemo;
+import com.rustfisher.tutorial2020.style.XMLShapeDemo;
 
 public class MainActivity extends AbsActivity implements View.OnClickListener {
 
@@ -14,7 +16,8 @@ public class MainActivity extends AbsActivity implements View.OnClickListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setOnClickListener(this, R.id.re_btn, R.id.animation_demo_btn, R.id.linear_layout_btn);
+        setOnClickListener(this, R.id.re_btn, R.id.animation_demo_btn, R.id.linear_layout_btn,
+                R.id.xml_shape_demo, R.id.color_list_demo_btn);
     }
 
     @Override
@@ -28,6 +31,12 @@ public class MainActivity extends AbsActivity implements View.OnClickListener {
                 break;
             case R.id.linear_layout_btn:
                 startActivity(new Intent(getApplicationContext(), LinearGuideAct.class));
+                break;
+            case R.id.xml_shape_demo:
+                startActivity(new Intent(getApplicationContext(), XMLShapeDemo.class));
+                break;
+            case R.id.color_list_demo_btn:
+                startActivity(new Intent(getApplicationContext(), LayoutBackgroundDemo.class));
                 break;
         }
     }
