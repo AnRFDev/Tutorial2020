5381ed2dd5fc28d72c90092e3d7c81d0cf487b7d
RustFisher <rustfisher@foxmail.com>
2020-11-02 11:42:42 +0800

    [http-conn] sample

--- app/src/main/java/com/rustfisher/tutorial2020/act/HttpUrlConnDemo1.java
+++ app/src/main/java/com/rustfisher/tutorial2020/act/HttpUrlConnDemo1.java
@@ -1,6 +1,7 @@
 package com.rustfisher.tutorial2020.act;
 
 import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
@@ -12,14 +13,18 @@ import androidx.annotation.Nullable;
 
 import com.rustfisher.tutorial2020.R;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public class HttpUrlConnDemo1 extends Activity {
     private static final String TAG = "rustAppHttpUrlConnDemo1";
-    private Handler mHandler = new Handler(Looper.getMainLooper());
     private WebView web1;
 
     @Override
@@ -33,28 +38,32 @@ public class HttpUrlConnDemo1 extends Activity {
                 new Thread(new Runnable() {
                     @Override
                     public void run() {
-                        reqInfo1();
+                        Log.d(TAG, "run: " + getSampleWhiteList());
                     }
                 }).start();
             }
         });
     }
 
-    private void reqInfo1() {
+    // 获取应用安装白名单信息
+    private List<String> getSampleWhiteList() {
         try {
-            URL url = new URL("https://www.baidu.com");
+            URL url = new URL("http://wlList");
 
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 
-            conn.setRequestMethod("GET");
+            conn.setRequestMethod("POST");
             conn.setConnectTimeout(10 * 1000);
 
             // 请求头的信息 这里需要平板厂商根据情况改参数
-            String body = "wd=" + "天气预报";
-            body="";
+            JSONObject bodyJson = new JSONObject();
+            bodyJson.put("imei", "imei");
+            bodyJson.put("deviceSn", "get-sn");
+            bodyJson.put("deviceBrand", Build.BRAND);
+            String body = bodyJson.toString();
+            Log.d(TAG, "getWhiteList: body: " + body);
 
-            Log.d(TAG, "reqInfo1: body: " + body);
-
+            conn.setRequestProperty("Content-Type", "application/json"); // 添加类型设置
             conn.setRequestProperty("Cache-Control", "max-age=0");
             conn.setDoOutput(true);
             conn.getOutputStream().write(body.getBytes());
@@ -68,22 +77,35 @@ public class HttpUrlConnDemo1 extends Activity {
                 while ((len = inputStream.read(in)) > -1) {
                     baos.write(in, 0, len);
                 }
-                final String content = new String(baos.toByteArray());
+                String content = new String(baos.toByteArray());
                 baos.close();
                 inputStream.close();
                 conn.disconnect();
 
-                Log.d(TAG, "reqInfo1: " + content);
-                mHandler.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        web1.loadDataWithBaseURL(null, content, "text/html", "utf-8", null);
+                JSONObject jsonObject = new JSONObject(content);
+                if (jsonObject.has("body")) {
+                    JSONObject bodyObj = jsonObject.getJSONObject("body");
+                    if (bodyObj.has("white")) {
+                        JSONArray whiteJsonArr = bodyObj.getJSONArray("white");
+                        if (whiteJsonArr.length() > 0) {
+                            List<String> whiteList = new ArrayList<>();
+                            for (int i = 0; i < whiteJsonArr.length(); i++) {
+                                String w = (String) whiteJsonArr.get(i);
+                                whiteList.add(w);
+                            }
+                            return whiteList;
+                        } else {
+                            Log.d(TAG, "getWhiteList: 白名单为空");
+                        }
                     }
-                });
+                }
+            } else {
+                Log.e(TAG, "getWhiteList: [" + code + "] " + conn.getResponseMessage());
             }
         } catch (Exception e) {
             e.printStackTrace();
-            Log.e(TAG, "reqInfo1: ", e);
+            Log.e(TAG, "getWhiteList: ", e);
         }
+        return null; // 白名单为空或者获取失败
     }
 }
