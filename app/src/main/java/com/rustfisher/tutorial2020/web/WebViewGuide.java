83f149c6bb64b7cfdc5a4d65d6dcbcdf151ec75d
RustFisher <rustfisher@foxmail.com>
2021-01-01 21:20:22 +0800

    webview local html

--- app/src/main/java/com/rustfisher/tutorial2020/web/WebViewGuide.java
+++ app/src/main/java/com/rustfisher/tutorial2020/web/WebViewGuide.java
@@ -21,7 +21,8 @@ public class WebViewGuide extends AbsGuideAct {
                 new GuideAdapter.OptionItem("demo1", "", true, WvDemo1Act.class, R.drawable.item_type_web),
                 new GuideAdapter.OptionItem("WebView长按和点击", "长按菜单", true, Wv2ClickAct.class, R.drawable.item_type_web),
                 new GuideAdapter.OptionItem("X5 WebView长按菜单", "需要进行一些操作", true, WebViewX5ClickAct.class, R.drawable.item_type_web),
-                new GuideAdapter.OptionItem("加载assets里的网页", "网页文件放在了assets目录里", true, WebViewLoadAssetsAct.class, R.drawable.item_type_web)
+                new GuideAdapter.OptionItem("加载assets里的网页", "网页文件放在了assets目录里", true, WebViewLoadAssetsAct.class, R.drawable.item_type_web),
+                new GuideAdapter.OptionItem("加载内部存储里的网页", "", true, WebViewLoadAppLocal1Act.class, R.drawable.item_type_web)
         ));
 
         mGuideAdapter.setOnClzListener(new GuideAdapter.OnClzListener() {
