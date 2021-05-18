a80ad0e6945a9ffb6c520b1927becf6ec18ffefb
RustFisher <rustfisher@foxmail.com>
2021-05-18 22:25:20 +0800

    gradle

--- app/src/main/java/com/rustfisher/tutorial2020/storage/room/AppDatabase.java
+++ app/src/main/java/com/rustfisher/tutorial2020/storage/room/AppDatabase.java
@@ -3,7 +3,7 @@ package com.rustfisher.tutorial2020.storage.room;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
-@Database(entities = {User.class, User2.class}, version = 2)
+@Database(entities = {User.class, User2.class}, version = 2, exportSchema = false)
 public abstract class AppDatabase extends RoomDatabase {
     public abstract UserDao userDao();
 }
