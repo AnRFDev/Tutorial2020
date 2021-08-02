0f6f7e498c3dcb281101a6e22291c4dacedb1d39
RustFisher <rustfisher@foxmail.com>
2021-08-02 11:15:58 +0800

    帧动画添加log

--- app/src/main/java/com/rustfisher/tutorial2020/animation/frame/FrameAnimationAct1.kt
+++ app/src/main/java/com/rustfisher/tutorial2020/animation/frame/FrameAnimationAct1.kt
@@ -3,12 +3,16 @@ package com.rustfisher.tutorial2020.animation.frame
 import android.app.Activity
 import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
 import com.rustfisher.tutorial2020.R
 
 
 class FrameAnimationAct1 : Activity() {
+    companion object {
+        const val TAG = "rustApp"
+    }
 
     var mFrameIv: ImageView? = null
 
@@ -21,10 +25,19 @@ class FrameAnimationAct1 : Activity() {
         findViewById<Button>(R.id.start_btn).setOnClickListener {
             val ani: AnimationDrawable = mFrameIv!!.background as AnimationDrawable
             ani.start()
+            logAnimationDrawable(ani)
         }
         findViewById<Button>(R.id.stop_btn).setOnClickListener {
             val ani: AnimationDrawable = mFrameIv!!.background as AnimationDrawable
             ani.stop()
+            logAnimationDrawable(ani)
+        }
+    }
+
+    private fun logAnimationDrawable(ad: AnimationDrawable) {
+        Log.d(TAG, "AnimationDrawable isOneShot: ${ad.isOneShot}, isRunning: ${ad.isRunning}, NumberOfFrames: ${ad.numberOfFrames}")
+        for (i in 0 until ad.numberOfFrames) {
+            Log.d(TAG, "frame[$i] duration: ${ad.getDuration(i)}")
         }
     }
 }
