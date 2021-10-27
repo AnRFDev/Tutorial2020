318790cc4e16cf19fc8bb10ea2dcc95604b05767
RustFisher <rustfisher@foxmail.com>
2021-10-27 08:21:37 +0800

    [okhttp] post1

--- app/src/main/java/com/rustfisher/tutorial2020/okhttp/OkHttpUse1.kt
+++ app/src/main/java/com/rustfisher/tutorial2020/okhttp/OkHttpUse1.kt
@@ -9,7 +9,11 @@ import androidx.databinding.DataBindingUtil
 import com.rustfisher.tutorial2020.R
 import com.rustfisher.tutorial2020.databinding.ActOkhttpUse1Binding
 import okhttp3.*
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.RequestBody.Companion.toRequestBody
+import org.json.JSONObject
 import java.io.IOException
+import kotlin.concurrent.thread
 
 /**
  * @author rustfisher
@@ -28,6 +32,9 @@ class OkHttpUse1 : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.act_okhttp_use1)
         binding.get1.setOnClickListener {
             get1()
+        }
+        binding.post1.setOnClickListener {
+            thread { post1() }
         }
     }
 
@@ -51,5 +58,37 @@ class OkHttpUse1 : AppCompatActivity() {
                 }
             }
         })
+    }
+
+    private fun post1() {
+        val mediaType = "application/json; charset=utf-8".toMediaType()
+        val client = OkHttpClient()
+        val postData = buildJson1()
+        val body = buildJson1().toString().toRequestBody(mediaType)
+        Log.d(TAG, "post1: $postData")
+        val request = Request.Builder().url("https://gitee.com/api/v5/gists")
+                .post(body)
+                .build()
+        val resp = client.newCall(request).execute() // 立刻执行 会阻塞当前线程
+        if (resp.code >= 200 || resp.code <= 201) {
+            val resStr = resp.body!!.string()
+            Log.d(TAG, "post1: resp body: $resStr")
+            mHandler.post { binding.resTv.text = resStr }
+        }
+    }
+
+    private fun buildJson1(): JSONObject {
+        val map = HashMap<String, Any>()
+        map["access_token"] = "0d6f65ef07976154138e126764303622"
+        val file1Map = HashMap<String, Any>()
+        file1Map["content"] = "# POST测试 \n rustfisher.com"
+        val file2Map = HashMap<String, Any>()
+        file2Map["content"] = "# 第二个文件 \n an.rustfisher.com"
+        val filesMap = HashMap<String, Any>()
+        filesMap["file1"] = file1Map
+        filesMap["file2"] = file2Map
+        map["files"] = filesMap
+        map["description"] = "测试POST接口"
+        return JSONObject(map as Map<*, *>)
     }
 }
